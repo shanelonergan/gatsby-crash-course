82e97a0385202aa7cd775fe84dbde0657d40909b
shanelonergan <sptlonergan@gmail.com>
2020-01-27 22:45:21 -0500

    blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const blog = () => {
+    return (
+        <div>
+            <h1>Blog</h1>
+            <p>Posts will show up here later on</p>
+        </div>
+    );
+}
+
+export default blog;
