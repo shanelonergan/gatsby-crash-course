b2eaa9dc437805c41b35fcf94a3ae0b5d9cfdef7
shanelonergan <sptlonergan@gmail.com>
2020-01-27 23:36:16 -0500

    footer

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const footer = () => {
+    return (
+       <footer>
+            <p>Created by Shane Lonergan © 2020</p>
+       </footer>
+    );
+}
+
+export default footer;
