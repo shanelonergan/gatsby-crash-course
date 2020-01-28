f3756a4c66770a94def3038bf378a42b46c8d680
shanelonergan <sptlonergan@gmail.com>
2020-01-28 11:14:10 -0500

    adjusted nav componen

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,14 +3,15 @@ import { Link } from 'gatsby'
 
 const nav = () => {
     return (
-        <div>
+        <header>
             <p>
+                Shane Lonergan |
                 <Link to="/"> Home</Link> |
                 <Link to="/about"> About</Link> |
                 <Link to="/contact"> Contact</Link> |
                 <Link to="/blog"> Blog</Link>
             </p>
-        </div>
+        </header>
     );
 }
 
