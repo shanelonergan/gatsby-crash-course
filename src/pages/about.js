40e6cc93a809ac059f471cbc85143178caf5391f
shanelonergan <sptlonergan@gmail.com>
2020-01-27 23:30:47 -0500

    link to contact from about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'gatsby'
 
 const about = () => {
     return (
         <div>
             <h1>About Me</h1>
             <p>I am a developer learning to use Gatsby.js. WOO!</p>
+            <br/>
+            <p><Link to='/contact'>Contact me</Link> for more information.</p>
         </div>
     );
 }
