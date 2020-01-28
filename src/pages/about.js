22ffa9d39519d5db66bb5a0d0542ed3c6c4eaa47
shanelonergan <sptlonergan@gmail.com>
2020-01-27 22:47:17 -0500

    about page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const about = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>I am a developer learning to use Gatsby.js. WOO!</p>
+        </div>
+    );
+}
+
+export default about;
