983652c34f2bf4e03737bff554f4e6a71ab4c070
shanelonergan <sptlonergan@gmail.com>
2020-01-28 12:41:35 -0500

    adding styling

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby'
+import navStyles from './nav.module.scss'
 
 const nav = () => {
     return (
         <header style={{'text-align':'center'}}>
             <p>
                 Shane Lonergan |
-                <Link to="/"> Home</Link> |
+                <Link to="/" className={navStyles.link}> Home</Link> |
                 <Link to="/about"> About</Link> |
                 <Link to="/contact"> Contact</Link> |
                 <Link to="/blog"> Blog</Link>
