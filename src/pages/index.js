3fc3a484c8e1d2b11e0c9d7f949764ea14f35220
shanelonergan <sptlonergan@gmail.com>
2020-01-27 23:11:25 -0500

    changed a tag to Link component

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <h2>I am Shane, a full-stack developer living in the big apple.</h2>
-    <p>Need a dev? <a href="/contact">Contact me!</a></p>
+    <p>Need a dev? <Link to="/contact">Contact me!</Link></p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
